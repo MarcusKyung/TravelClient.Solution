480a6f6e45cf259a7e3b41c5ec607322b89a0cc6
MarcusKyung <Kyungmj@gmail.com>
2023-06-07 13:04:33 -0700

    add ReviewsController() routes

---
+++ TravelClient/Controllers/ReviewsController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using TravelClient.Models;
+
+namespace TravelClient.Controllers;
+
+public class ReviewsController : Controller
+{
+  public IActionResult Index()
+  {
+    List<Review> reviews = Review.GetReviews();
+    return View(reviews);
+  }
+
+  public IActionResult Details(int id)
+  {
+    Review review = Review.GetDetails(id);
+    return View(review);
+  }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Review review)
+  {
+    Review.Post(review);
+    return RedirectToAction("Index");
+  }
+
+  public ActionResult Edit(int id)
+  {
+    Review review = Review.GetDetails(id);
+    return View(review);
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Review review)
+  {
+    Review.Put(review);
+    return RedirectToAction("Details", new { id = review.ReviewId});
+  }
+
+  public ActionResult Delete(int id)
+  {
+    Review review = Review.GetDetails(id);
+    return View(review);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Review.Delete(id);
+    return RedirectToAction("Index");
+  }
+}
